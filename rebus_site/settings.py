7a203b90c2b9b5ccb84771bea676673e0c3d0acf
Mariana Bustamante <marianabb@gmail.com>
2011-09-07 19:40:08 +0200

    Small changes to deply in Djangozoom

--- rebus_site/settings.py
+++ rebus_site/settings.py
@@ -55,7 +55,7 @@ MEDIA_URL = '/media/' #TODO change when appropriate
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = '/'
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
